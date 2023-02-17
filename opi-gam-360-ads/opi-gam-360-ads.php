c09c579bb3722ebd108a0b5642c57e9e73628aff
Jordan Ng <jng@staradvertiser.com>
2023-02-16 14:10:45 -1000

    added readme.txt

--- opi-gam-360-ads/opi-gam-360-ads.php
+++ opi-gam-360-ads/opi-gam-360-ads.php
@@ -3,7 +3,7 @@
  * Plugin Name:       OPI GAM 360 Ads
  * Plugin URI:        https://oahupublications.com/
  * Description:       Provides a UI and plugin based method to implement GAM 360 Ads on a site. 
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Oahu Publications Inc.
  * Author URI:        https://oahupublications.com/
  */
@@ -12,6 +12,21 @@
 if ( ! defined( 'WPINC' ) ) {
     die;
 }
+
+require 'plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/JordanNg/opi-gam-360-plugin/',
+	__FILE__,
+	'opi-gam-360-ads'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('production');
+
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('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');
 
 /**
  * Require the core class for ad functionality
